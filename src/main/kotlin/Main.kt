f72d58b2c6b4a1332cab36cd58e32be3aec9df0c
Eloy <eesteban@iesrafaelalberti.es>
2024-02-20 23:58:48 +0100

    Creacion de Main.kt/Documentación de Stack.kt y Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,46 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+/**
+ * Función genérica que invierte el orden de los elementos en una Iterable.
+ *
+ * @param list Iterable que se desea invertir.
+ * @return Lista con los elementos en orden inverso.
+ */
+fun <T> reverse(list: Iterable<T>): List<T> {
+    // Se crea una instancia de la clase Stack para almacenar los elementos de la Iterable en orden inverso.
+    val stack = Stack<T>()
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    // Se recorre la Iterable y se agregan los elementos a la pila.
+    for (elemento in list) {
+        stack.push(elemento)
+    }
+
+    // Se crea una lista mutable para almacenar los elementos en orden inverso.
+    val reversed = mutableListOf<T>()
+
+    // Se desapilan los elementos y se agregan a la lista en orden inverso.
+    while (!stack.vacia()) {
+        stack.pop()?.let { reversed.add(it) }
+    }
+
+    // Se devuelve la lista con los elementos en orden inverso.
+    return reversed
+}
+
+/**
+ * Función principal que demuestra el uso de la función reverse.
+ */
+fun main() {
+    // Se crea una lista de strings.
+    var numbers = listOf("one", "two", "three", "four")
+
+    // Se invierte la lista utilizando la función reverse.
+    var numbersRev = reverse(numbers)
+
+    // Se verifica si la lista invertida es igual a la lista esperada ("four", "three", "two", "one").
+    if (!listOf("four", "three", "two", "one").equals(numbersRev))
+        println("Error")
+    else
+        println("Correcto")
+
+    // Se imprime la lista invertida.
+    println(numbersRev)
 }
--- src/main/kotlin/Stack.kt
+++ src/main/kotlin/Stack.kt
@@ -1,20 +1,49 @@
-class Stack<T>{
+/**
+ * Clase genérica que implementa una pila (stack) en Kotlin.
+ *
+ * @param T Tipo de elementos que se almacenarán en la pila.
+ */
+class Stack<T> {
 
+    // Lista mutable que almacena los elementos de la pila.
     private val elementos = mutableListOf<T>()
-    fun tope(): T?{
+
+    /**
+     * Devuelve el elemento en la cima de la pila sin eliminarlo.
+     *
+     * @return Elemento en la cima de la pila o null si la pila está vacía.
+     */
+    fun tope(): T? {
         return elementos.lastOrNull()
     }
 
-    fun push(elemento: T){
+    /**
+     * Agrega un elemento a la cima de la pila.
+     *
+     * @param elemento Elemento que se va a agregar a la pila.
+     */
+    fun push(elemento: T) {
         elementos.add(elemento)
     }
 
-    fun pop(): T?{
-        return if (elementos.isNotEmpty()) elementos.removeAt(elementos.size-1) else null
+    /**
+     * Elimina y devuelve el elemento en la cima de la pila.
+     *
+     * @return Elemento en la cima de la pila o null si la pila está vacía.
+     */
+    fun pop(): T? {
+        // Verifica si la pila no está vacía antes de intentar eliminar el elemento.
+        return if (elementos.isNotEmpty()) elementos.removeAt(elementos.size - 1) else null
     }
 
-    fun vacia(): Boolean{
+    /**
+     * Verifica si la pila está vacía.
+     *
+     * @return true si la pila está vacía, false si contiene elementos.
+     */
+    fun vacia(): Boolean {
         return elementos.isEmpty()
     }
 }
+
 
